cf3fa1ee5309ae985f3eee4e564207e0f100860e
Arth Sonani <115365732+ArthSonani@users.noreply.github.com>
2024-08-21 23:41:52 +0530

    Update Shop.jsx

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -14,7 +14,7 @@ export default function Shop() {
 
   async function getNearStores(){
     try{
-      const res = await fetch('/api/shop/near-store', {
+      const res = await fetch('https://freshco-0dlm.onrender.com/api/shop/near-store', {
         method : 'POST',
         headers : { 
           'Content-Type': 'application/json'
